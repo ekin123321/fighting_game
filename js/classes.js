2a0b50ef5b9dac42385959e32be274ede94c5441
ekin123321 <120812084+ekin123321@users.noreply.github.com>
2025-07-01 15:40:37 +0300

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -279,8 +279,16 @@ class Bomb {
             ctx.strokeStyle = color;
             ctx.lineWidth = 2;
             ctx.globalAlpha = 0.8;
-            setTimeout(() => {
-                ctx.globalAlpha = 1;
+          setTimeout(() => {
+              
+              setInterval(() => {
+                  if (c) {
+                     ctx.globalAlpha = ctx.globalAlpha+0.01
+                    if (ctx.globalAlpha == 1) {
+                        c = false
+                    }
+                  }                  
+                }, 100);               
             }, 10000);
             
             // Outer circle
